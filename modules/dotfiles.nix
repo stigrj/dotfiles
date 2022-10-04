d10a237df7d46b066fc7084a2107e53e62862951
Stig Rune Jensen <stig.r.jensen@oceanbox.io>
2022-10-04 09:59:50 +0200

    Update vim settings

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -346,6 +346,7 @@ let
           neco-ghc
           purescript-vim
           rust-vim
+          vim-fsharp
           # vim-ionide
           # vim-clojure-static
           # vim-clojure-highlight
